e313458a4d48b56325797724526af077ca3bb2ba
Liza <korotchenko.liza@gmail.com>
2016-02-13 13:21:33 +0100

    fixed from in mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,13 @@
 class ContactsController < ApplicationController
 
   def send_mail
-    if params[:name] && params[:email] && params[:phone] && params[:message]
-      contact = Contact.new
-      contact.name = params[:name]
-      contact.email = params[:email]
-      contact.phone = params[:phone]
-      contact.message = params[:message]
-      contact.save
-      ContactMailer.contact_email(contact).deliver
-    end
+    contact = Contact.new
+    contact.name = params[:name]
+    contact.email = params[:email]
+    contact.phone = params[:phone]
+    contact.message = params[:message]
+    contact.save
+    ContactMailer.contact_email(contact).deliver
     redirect_to root_path
   end
-
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,10 @@
 class ContactMailer < ApplicationMailer
   default to: "korotchenko.liza@gmail.com"
+  default from: "contact@example.com"
 
   def contact_email(contact)
     @contact = contact
-    mail(from: contact.email, subject: 'Someone writes you!')
+    mail(subject: 'Someone writes you!')
   end
 
 end
