fc6b1054de31f4b611b485fa9afa4486a8beb6e2
Liza <korotchenko.liza@gmail.com>
2016-02-13 13:08:59 +0100

    fixed the contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,15 @@
 class ContactsController < ApplicationController
 
   def send_mail
-    contact = Contact.new
-    contact.name = params[:name]
-    contact.email = params[:email]
-    contact.phone = params[:phone]
-    contact.message = params[:message]
-    ContactMailer.contact_email(contact).deliver
+    if params[:name] && params[:email] && params[:phone] && params[:message]
+      contact = Contact.new
+      contact.name = params[:name]
+      contact.email = params[:email]
+      contact.phone = params[:phone]
+      contact.message = params[:message]
+      contact.save
+      ContactMailer.contact_email(contact).deliver
+    end
     redirect_to root_path
   end
 
